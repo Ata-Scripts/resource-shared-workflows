7cb7d9250adeb6c55dbc5fff416b95d1b4f1fd6a
Ata Revals <47323005+atarevals@users.noreply.github.com>
2025-10-08 05:37:47 +0330

    Create bump-manifest-version.js

---
+++ .github/actions/bump-manifest-version.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const path = require('path');
+const replace = require('replace-in-file');
+
+const version = process.env.TGT_RELEASE_VERSION;
+const newVersion = version.replace("v", "");
+const defaultFolderPath = process.env.GITHUB_WORKSPACE;
+const targetFile = 'fxmanifest.lua';
+const allResults = [];
+
+const replaceInFiles = (folderPath) => {
+  const entries = fs.readdirSync(folderPath, { withFileTypes: true });
+
+  entries.forEach(entry => {
+    const entryPath = path.join(folderPath, entry.name);
+      console.log(entryPath)
+    if (entry.isDirectory()) {
+      console.log("replace", entryPath)
+      replaceInFiles(entryPath);
+    } else if (entry.isFile() && entry.name === targetFile) {
+      const options = {
+        files: entryPath,
+        from: /\bversion\s+(.*)$/gm,
+        to: `version '${newVersion}'`,
+      };
+
+      try {
+        const results = replace.sync(options);
+        results.forEach(result => {
+          if (result.hasChanged) {
+            allResults.push(result);
+          }
+        });
+      } catch (error) {
+        console.error('Error occurred:', error);
+      }
+    }
+  });
+};
+
+//replaceInFiles(defaultFolderPath);
+
+const results = replace.sync({
+    files: "./**/fxmanifest.lua",
+    from: /\bversion\s+(.*)$/gm,
+    to: `version '${newVersion}'`,
+});
+
+process.on('exit', () => {
+  const changedFiles = results.map(result => result.file);
+  console.log(changedFiles);
+  //console.log(defaultFolderPath);
+  //if (changedFiles.length > 0) {
+    //console.log(`echo "FXMANIFEST_FILE_CHANGES=${JSON.stringify(changedFiles)}" >> $GITHUB_ENV`);
+  //} else {
+    //console.log('No changes!');
+  //}
+});
